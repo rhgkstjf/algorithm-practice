f7efe12761885d1cabef2b8819bad8441fcd284f
rhgkstjf <44472886+rhgkstjf@users.noreply.github.com>
2020-05-06 01:06:24 +0900

    Update 2839.cpp
    수정중

--- 2839.cpp
+++ 2839.cpp
@@ -9,24 +9,32 @@ int main()
 	if (N >= 3 && N <= 5000)
 	{
 		int num = -1;
-		if ((N % 5) % 3 != 0 && ((N % 3 != 0) && (N % 5 != 0)))
+		if ((N % 5) % 3 != 0 && ((N % 3 != 0) && (N % 5 != 0)) && (((N%5)+5)%3 != 0 && N > 10))
 		{
 			cout << num << endl;
 		}
-		else if(N%3 == 0){
-			int p = N / 3;
-			cout << p << endl;
+		else if (((N % 5) + 5) % 3 == 0 && N > 10)
+		{
+			int p = (N / 5) - 1;
+			int q = ((N % 5) + 5) / 3;
+			cout << p + q << endl;
+		}
+		else if ((N % 5) % 3 == 0) {
+			int p = N / 5;
+			int q = (N % 5) / 3;
+			cout << p + q << endl;
 		}
 		else if (N % 5 == 0)
 		{
 			int p = N / 5;
 			cout << p << endl;
 		}
-		else {
-			int p = N / 5;
-			int q = (N % 5) / 3;
-			cout << p + q << endl;
+		else if (N % 3 == 0) {
+			int p = N / 3;
+			cout << p << endl;
 		}
+		else
+			cout << num<<endl;
 	}
 
 	return 0;
