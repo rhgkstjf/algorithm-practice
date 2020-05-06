d3b54bd162525dfc31c7d8bdccba50d42251a04b
rhgkstjf <44472886+rhgkstjf@users.noreply.github.com>
2020-05-06 16:39:27 +0900

    Update 1193.cpp
    해결완료!

--- 1193.cpp
+++ 1193.cpp
@@ -5,29 +5,22 @@ int main()
 {
 	int X;
 	cin >> X;
-	if (X >= 1 &&  X <= 10000000)
+	if (X >= 1 && X <= 10000000)
 	{
-		
 		int i = 1;
 		int j = 1;
-		int A = 1;
-		for (; i < 10000000; i+=j)
+		int A = 0;
+		for (; i <= X; i += j)
 		{
-			if (i > X)
-			{
-				int c = X - A;
-				if(j%2 ==0)
-					cout << (1 + c) << "/" << (j - c) << endl;
-				else if(j%2 !=0)
-					cout << (j - c) << "/" << (1 + c) << endl;
-				break;
-			}
-			else {
-				A = i;
-				if (i != 1)
-					j++;
-			}
+			A = i;
+			if (i != 1) { j++; }
+
 		}
+		int c = X - A;
+		if (j % 2 == 0)
+			cout << (1 + c) << "/" << (j - c) << endl;
+		else if (j % 2 != 0)
+			cout << (j - c) << "/" << (1 + c) << endl;
 	}
 
 	return 0;
