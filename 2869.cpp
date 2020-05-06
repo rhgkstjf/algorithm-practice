47cbffb47829ce1d591046c9351cf87eff990c65
rhgkstjf <44472886+rhgkstjf@users.noreply.github.com>
2020-05-06 17:08:18 +0900

    Update 2869.cpp
    해결

--- 2869.cpp
+++ 2869.cpp
@@ -7,8 +7,11 @@ int main()
 	cin >> A >> B >> V;
 	if (B >= 1 && B < A && A <= V && V <= 1000000000)
 	{
-		int i = (V - A) / (A - B);
-		cout << i+1 << endl;
+		int i = (V - B) / (A - B);
+		if ((V - B) % (A - B) == 0)
+			cout << i << endl;
+		else
+			cout << i+1 << endl;
 	}
 
 	return 0;
